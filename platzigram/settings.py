245e32921da10b3bb8ca03afa0a38d46cff77f52
Daniel Salazar <daniel.salazarop@gmail.com>
2021-01-27 18:49:19 -0600

    Update settings.py

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_s0z)6w4k8ra8l_$3menl+5m+1x$c+=tk%-rnvlq##=j_!paox'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
